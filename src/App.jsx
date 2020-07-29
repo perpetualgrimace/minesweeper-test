1029d321a0d7a70ed166ccc93b8aea4f380a73fa
perpetualgrimace <hello@jamesferrell.me>
2020-07-29 15:25:20 -0400

    randomly adds mines onMount; displays tiles as buttons with debug text

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,57 @@ import defaultTiles from './defaultTiles.json';
 import './App.css';
 
 export default class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      tiles: defaultTiles,
+      mineCount: 10
+    };
+  }
+
+  getRandomTile(tiles) {
+  	return Math.floor(Math.random() * tiles.length);
+  }
+
+  // TODO: account for when the same number is randomly picked twice 
+  addMines() {
+  	const {mineCount, tiles} = this.state;
+
+  	let updatedTiles = tiles;
+  	let i = 0;
+
+  	for (i; i < mineCount; i++) {
+  		updatedTiles[this.getRandomTile(tiles)].mine = true;
+  	}
+
+  	this.setState({tiles: updatedTiles});
+  }
+
+  componentDidMount() {
+    this.addMines();
+  }
+
   render() {
-    // console.log(defaultTiles);
+    const {tiles} = this.state;
+
+    // console.log(tiles);
+    // console.log(tiles.filter(t => t.mine === true));
+
+    // defaultTiles = [ { row: number, col: number, clicked: false, mine: false } ... ];
 
     return (
       <div className="ms">
-        <h1>Now I'm classy</h1>
+        <div className="ms-grid">
+          {tiles.map((tile, i) =>
+            <button className="ms-tile" key={`${tile.row} : ${tile.col}`}>
+              <span className="u-visually-hidden">
+                {tile.row} : {tile.col}
+              </span>
+              {tile.mine ? '💣' : ''}
+            </button>
+          )}
+        </div>
       </div>
     );
   }
